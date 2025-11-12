09284ac07c7bc5328b1b70312242088364de84bc
alfian nurhikmat <if24.alfiannurhikmat@mhs.ubpkarawang.ac.id>
2025-11-12 23:05:59 +0700

    pertemuan6

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
 
-require_once('./LuasLingkaran.php'); //cara panggil file
+namespace App\Math;
 
-use App\Math\LuasLingkaran; //panggil class
+class LuasLingkaran {
 
-$lingkaran = new LuasLingkaran(10);
-// $lingkaran->jari = 12;
-$lingkaran->tampil('roda'); //panggil method tampil
+    public const phi = 3.14;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
+    }
 
-LuasLingkaran::testing(); //panggil static method
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
+}
